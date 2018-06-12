27f46053a529a00aeace39692b2e6765259df0dd
b03901161yang <b03901161@ntu.edu.tw>
2018-06-12 13:15:16 +0800

    sh

---
+++ hw5/hw5_p1.sh
@@ -0,0 +1,3 @@
+wget 'https://www.dropbox.com/s/460vcn37pr5u66r/hw5_p1_cnn.pt?dl=1' -O 'hw5_p1_cnn.pt'
+wget 'https://www.dropbox.com/s/uxvjx7bf1219yrt/hw5_p1_fc.pt?dl=1' -O 'hw5_p1_fc.pt'
+python3 hw5_p1_test.py $1 $2 $3
---
+++ hw5/hw5_p2.sh
@@ -0,0 +1,2 @@
+wget 'https://www.dropbox.com/s/li8ftps4zp1xorr/hw5_p2_cnn.pt?dl=1' -O 'hw5_p2_cnn.pt'
+python3 hw5_p2_test.py $1 $2 $3
